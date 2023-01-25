3c87fa5b9dcdb2d1a76cef04493a79bb5cc36e50
kreatorland <kreatorland@gmail.com>
2023-01-25 21:29:55 +0545

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,124 @@
+import { exec as _exec } from 'child_process'
+
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-solhint'
+import '@nomiclabs/hardhat-truffle5'
+import '@nomiclabs/hardhat-waffle'
+import dotenv from 'dotenv'
+import 'hardhat-abi-exporter'
+import 'hardhat-deploy'
+import 'hardhat-gas-reporter'
+import { HardhatUserConfig, task } from 'hardhat/config'
+import { Artifact } from 'hardhat/types'
+import { promisify } from 'util'
+import "hardhat-contract-sizer";
+
+const exec = promisify(_exec)
+
+// hardhat actions
+import './tasks/accounts'
+import './tasks/archive_scan'
+import './tasks/save'
+import './tasks/seed'
+
+// Load environment variables from .env file. Suppress warnings using silent
+// if this file is missing. dotenv will never modify any environment variables
+// that have already been set.
+// https://github.com/motdotla/dotenv
+dotenv.config({ debug: false })
+
+let real_accounts = undefined
+if (process.env.DEPLOYER_KEY) {
+  real_accounts = [process.env.DEPLOYER_KEY, process.env.OWNER_KEY || process.env.DEPLOYER_KEY]
+}
+
+// circular dependency shared with actions
+export const archivedDeploymentPath = './deployments/archive'
+
+const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      saveDeployments: false,
+      tags: ['test', 'legacy', 'use_root'],
+      allowUnlimitedContractSize: false,
+    },
+    polygon:{
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/6hN4YXfDvXUfoBNplsSGCl25nNUDVjkO',
+      accounts: ['b0c8e589239d45d9ed7c67f6a63719d464414cca20a38445612de09bdbd3649b'],
+      tags: ['test', 'legacy', 'use_root'],
+      chainId: 80001,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      tags: ['test', 'legacy', 'use_root'],
+      chainId: 4,
+      accounts: real_accounts,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      tags: ['test', 'legacy', 'use_root'],
+      chainId: 3,
+      accounts: real_accounts,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      tags: ['test', 'legacy', 'use_root'],
+      chainId: 5,
+      accounts: real_accounts,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      tags: ['legacy', 'use_root'],
+      chainId: 1,
+      accounts: real_accounts,
+    },
+  },
+  mocha: {},
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2500,
+          },
+        },
+      },
+    ],
+  },
+  abiExporter: {
+    path: './build/contracts',
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    except: [
+      'Controllable$',
+      'INameWrapper$',
+      'SHA1$',
+      'Ownable$',
+      'NameResolver$',
+      'TestBytesUtils$',
+      'legacy/*',
+    ],
+    spacing: 2,
+    pretty: true,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    owner: {
+      default: 0,
+    },
+  },
+  external: {
+    contracts: [
+      {
+        artifacts: [archivedDeploymentPath],
+      },
+    ],
+  },
+}
+
+export default config
